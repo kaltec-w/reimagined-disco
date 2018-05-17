82007d2f7475b93b391091b7b84cd357424c2dff
kaltec <38090454+kaltec-w@users.noreply.github.com>
2018-05-16 17:55:40 -0700

    Restructe
    Restructured, added more folders.

--- install.sh
+++ install.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-# Install pre-reqs via Bash
+# CURL Install
 
+echo "Installing rDisco - LibreNMS Edition"
 yum upgrade -y 1>/dev/null 2>&1
 yum install epel-release -y 1>/dev/null 2>&1
 yum install dialog git wget vim whiptail -y 1>/dev/null 2>&1
-
 yum install ansible -y 1>/dev/null 2>&1
-
 yum upgrade -y 1>/dev/null 2>&1
 
-# Start calling ansible playbooks
+rm -r /opt/rdisco/librenms
 
