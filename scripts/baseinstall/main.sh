919a77cc2c0640cb54324bdc41337b3860a64b5c
kaltec <38090454+kaltec-w@users.noreply.github.com>
2018-05-17 11:41:23 -0700

    added sleeps for testing

--- scripts/baseinstall/main.sh
+++ scripts/baseinstall/main.sh
@@ -17,25 +17,30 @@ fi
 ############################################################ Starting Installation
 echo "8" | dialog --gauge "Conducting a System Update" 7 50 0
 yum upgrade -y 1>/dev/null 2>&1
+sleep 5
 
 echo "16" | dialog --gauge "Enabling System Health Monitoring" 7 50 0
 yum install sysstat nmon -y 1>/dev/null 2>&1
 sed -i 's/false/true/g' /etc/default/sysstat 1>/dev/null 2>&1
+sleep 5
 
 ############################################################ Ansible Installation
 echo "32" | dialog --gauge "Installing: Ansible Playbook" 7 50 0
 yum install ansible -y 1>/dev/null 2>&1
+sleep 5
 
 ############################################################ Start of Role Execution
 echo "40" | dialog --gauge "Installing: rDisco Dependencies" 7 50 0
 ansible-playbook /opt/rdisco/ansible/rdisco.yml --tags preinstall 1>/dev/null 2>&1
+sleep 5
 
 echo "48" | dialog --gauge "Installing: rDisco Commands" 7 50 0
 ansible-playbook /opt/rdisco/ansible/rdisco.yml --tags commands &>/dev/null &
-sleep 2
+sleep 5
 
 echo "56" | dialog --gauge "Installing: rDisco MOTD" 7 50 0
 ansible-playbook /opt/rdisco/ansible/rdisco.yml --tags motd 1>/dev/null 2>&1
+sleep 5
 
 ############################################################ Docker Install
 docker --version | awk '{print $3}' > /var/rdisco/docker.version
@@ -44,28 +49,33 @@ docker_var=$( cat /var/rdisco/docker.version )
 if [ "$docker_var" == "18.03.1-ce," ]
 then
   echo "64" | dialog --gauge "Docker Is Already Installed" 7 50 0
-  sleep 1
+  sleep 5
 else
   echo "64" | dialog --gauge "Installing: Docker 18.03 (Please Be Patient)" 7 58 0
   ansible-playbook /opt/rdisco/ansible/rdisco.yml --tags docker 1>/dev/null 2>&1
+  sleep 5
 fi
 
 ############################################################ Portainer Install
 echo "72" | dialog --gauge "Installing: Portainer" 7 50 0
 ansible-playbook /opt/rdisco/ansible/rdisco.yml --tags portainer &>/dev/null &
-
+sleep 5
 
 echo "80" | dialog --gauge "Forcing Reboot of Existing Containers!" 7 50 0
 bash /opt/rdisco/scripts/containers/reboot.sh &>/dev/null &
+sleep 5
 
 echo "88" | dialog --gauge "Installing: WatchTower" 7 50 0
 ansible-playbook /opt/rdisco/ansible/rdisco.yml --tags watchtower &>/dev/null &
+sleep 5
 
 echo "96" | dialog --gauge "Installing: Python Support" 7 50 0
 bash /opt/rdisco/scripts/baseinstall/python.sh 1>/dev/null 2>&1
+sleep 5
+
 
 echo "100" | dialog --gauge "Installation Complete!" 7 50 0
-sleep 2
+sleep 5
 
 cat /var/rdisco/rdisco.preinstall > /var/rdisco/rdisco.preinstall.stored
 clear
